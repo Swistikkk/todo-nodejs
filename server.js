130ee94f33eaed8c24704df7c7293e76400b44c5
Виктор Оберемко <victor@MacBook-Pro.Guest-NET.local>
2018-10-30 14:40:23 +0300

    Added server file and index html

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+
+app.get('*', (req, res) => {
+   res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(port);
